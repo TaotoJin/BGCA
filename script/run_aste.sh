0a949c929ed06d7f8fa207f3d7c5a377615f45eb
JINNII@JoeyPC <taotojin@gmail.com>
2023-12-06 16:34:16 +0700

    reduce eval batch size

--- script/run_aste.sh
+++ script/run_aste.sh
@@ -13,7 +13,7 @@ python main.py --task aste \
             --n_gpu 0 \
             --train_batch_size 16 \
             --gradient_accumulation_steps 2 \
-            --eval_batch_size 128 \
+            --eval_batch_size 16 \
             --learning_rate 3e-4 \
             --num_train_epochs 25 \
             --save_last_k 3 \
